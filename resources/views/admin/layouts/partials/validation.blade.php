a6760f489bb9e73c2f90979b6bfee2159e9ee2aa
yingjhen <jhenlin0220@gmail.com>
2020-01-14 20:37:46 +0800

    加入admin(包含樣板)

---
+++ resources/views/admin/layouts/partials/validation.blade.php
@@ -0,0 +1,18 @@
+@if($errors->any())
+
+    <div class="row">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="alert alert-danger alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
+                    @foreach($errors->all() as $errors)
+                        <li>{{$errors}}</li>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+        <!-- /.row -->
+@endif
+
