47613a00bdd96f3ad5fcd1080c3c6ba716974001
PengHueiSin <eason12326@gmail.com>
2020-01-14 13:44:03 +0800

    註冊頁面修改

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,20 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="type" class="col-md-4 col-form-label text-md-right">{{ __('Type') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="type" type="checkbox" class="form-control @error('type') is-invalid @enderror" name="type" value="1" required autocomplete="gender" style="width: 15px; height: 15px">學生
+
+                                @error('type')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
