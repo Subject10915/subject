3694bebf2ec627c21c911fb9828e6bfdfdb977e6
yingjhen <jhenlin0220@gmail.com>
2020-01-14 15:22:22 +0800

    設定關聯(user、room、sta)

--- app/Sta.php
+++ app/Sta.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Sta extends Model
 {
     //
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function rooms()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
