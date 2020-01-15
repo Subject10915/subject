0c16bd51aca57e5fb06d35723867a53168e6811a
PengHueiSin <eason12326@gmail.com>
2020-01-15 14:41:37 +0800

    資料表內容

---
+++ sql/grades.sql
@@ -0,0 +1,11 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `grades` (`id`, `name`, `count`, `created_at`, `updated_at`) VALUES
+(1,	'四資一甲',	48,	NULL,	NULL),
+(2,	'四資一乙',	50,	NULL,	NULL),
+(3,	'四資二甲',	46,	NULL,	NULL),
+(4,	'四資二乙',	46,	NULL,	NULL),
+(5,	'四資三甲',	48,	NULL,	NULL),
+(6,	'四資三乙',	50,	NULL,	NULL),
+(7,	'四資四甲',	46,	NULL,	NULL),
+(8,	'四資四乙',	50,	NULL,	NULL);
---
+++ sql/users.sql
@@ -0,0 +1,8 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `type`) VALUES
+(1,	'姚佳心',	's3a632055@gm.student.ncut.edu.tw',	NULL,	'$2y$10$wJKf1jZjONyHjhYc7LOTO.FX5fSgmlUqkvJA4F.xV4d4U8KFgiSpS',	NULL,	'2020-01-14 14:10:40',	'2020-01-14 14:10:40',	1),
+(2,	'林盈甄',	's3a632059@gm.student.ncut.edu.tw',	NULL,	'$2y$10$3DhCx4BrMpF62LZRXwdZ5Oq7m0TwJbDc97H8miv6.SWgX461ZlDtW',	NULL,	'2020-01-14 14:11:25',	'2020-01-14 14:11:25',	1),
+(3,	'蔡欣如',	's3a632065@gm.student.ncut.edu.tw',	NULL,	'$2y$10$scLntdV7RSvh.toPISBA1eFmASbv4z6k6L4S8AcH.117yYpSdvCX.',	NULL,	'2020-01-14 14:11:59',	'2020-01-14 14:11:59',	1),
+(4,	'彭惠昕',	's3a632068@gm.student.ncut.edu.tw',	NULL,	'$2y$10$iqXjXjvbpCqWhzKbmbvHa.8Dbw17mm1qpQRXR7vaKG8pmCVz67M72',	NULL,	'2020-01-14 14:12:46',	'2020-01-14 14:12:46',	1),
+(6,	'巫秀文',	'witch@gmail.com',	NULL,	'$2y$10$JaOmvNCExlvYJN6MeFf6PeJ5VlHxmOY/56hLcxdVv5kZVd.hpDYo6',	NULL,	'2020-01-14 14:16:35',	'2020-01-14 14:16:35',	0);
