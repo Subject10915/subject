ec4e51e5867d49d5a3ecd27549fe9f2907b739bd
PengHueiSin <eason12326@gmail.com>
2020-05-27 19:48:23 +0800

    學生課表建立

---
+++ app/Http/Controllers/StudentitemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Studentitem;
+use Illuminate\Http\Request;
+
+class StudentitemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Studentitem  $studentitem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Studentitem $studentitem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Studentitem  $studentitem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Studentitem $studentitem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Studentitem  $studentitem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Studentitem $studentitem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Studentitem  $studentitem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Studentitem $studentitem)
+    {
+        //
+    }
+}
---
+++ resources/views/timetable/student.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.master')
+
+@section('title', '課表')
+
+@section('content')
+
+    <header class="masthead" style="background-color:#b0d4f1;">
+        <table style="height: 80px">
+            <tr>
+                <td></td>
+            </tr>
+        </table>
+    </header>
+
+
+    <body style="background-color:#b0d4f1;">
+    <center>
+        <table style="width: 70%">
+            <tr>
+                <td><h1>課表</h1></td>
+            </tr>
+        </table>
+        <table border="1" style="width: 70%">
+
+        </table>
+        <br>
+    </center>
+    </body>
+
+@endsection
