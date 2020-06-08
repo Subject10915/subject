cd4031014ec489fa33eceed7dcafea945f531554
xinru <s3a632065@student.ncut.edu.tw>
2020-06-09 01:17:17 +0800

    新增detect 資料表

---
+++ database/factories/DetectFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Detect;
+use Faker\Generator as Faker;
+
+$factory->define(Detect::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ routes/test.php
@@ -0,0 +1,10 @@
+<?php
+
+//$var = $_GET['varname'];
+$test = $_GET['test'];
+
+$detect = $_GET['detect'];
+$time = $_GET['time'];
+
+echo $time;
+echo $detect;
