0a323e8b6ba9bee649c7c3159da546bb0485b54c
PengHueiSin <eason12326@gmail.com>
2020-06-14 12:40:00 +0800

    測試用網頁

---
+++ resources/views/test.blade.php
@@ -0,0 +1,20 @@
+@php
+    /*$url= 'https://api.thingspeak.com/channels/1080631/fields/1.json?timezone=Asia/Taipei&results=2';
+    $contents =file_get_contents($url);
+
+    $NewString=preg_split('/,/',$contents);
+
+    if($NewString[13] == '"field1":"1"}]}')
+        print ("have person");
+    else
+        print ("no one");*/
+     //$field =$_GET['field1'];
+    //echo $field;
+    /*$datetime =date_create()->format('Y-m-d H:i:s');
+    $query ="INSERT INTO detects (field, datetime) VALUES ('$field', '$datetime')";
+
+    $result=*/
+    /*echo  $contents;
+    print_r($NewString);*/
+@endphp
+
